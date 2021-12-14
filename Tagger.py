6a9c7a40a0de43bfc4851c9c30141ea39ed25cb4
VentusBey <96052473+VentusBey@users.noreply.github.com>
2021-12-14 16:01:26 +0400

    Update Tagger.py

--- Tagger.py
+++ Tagger.py
@@ -293,31 +293,5 @@ async def mentionall(tagadmin):
 		sleep(0.5)
 
 
-def get_arg(message):
-    msg = message.text
-    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
-    split = msg[1:].replace("\n", " \n").split(" ")
-    if " ".join(split[1:]).strip() == "":
-        return ""
-    return " ".join(split[1:])
-
-
-@client.on(events.NewMessage(from_users=OWNER_ID, pattern="^/broadcast$"))
-async def broadcast(client, message):
-    to_send = get_arg(message)
-    chats = load_chats_list()
-    success = 0
-    failed = 0
-    for chat in chats:
-        try:
-            await brend.send_message(int(chat), to_send)
-            success += 1
-        except:
-            failed += 1
-            remove_chat_from_db(str(chat))
-            pass
-    await message.reply(f"Mesaj {success} gruba gönderildi. {failed} grub başarısız")
-
-
 print(">> Bot çalıyor merak etme 🚀 @lucimarka bilgi alabilirsin <<")
 client.run_until_disconnected()
